4e0604337cefe228e070760e48380ba841d4ab69
davidecrs <davidecrs92@gmail.com>
2023-05-12 10:11:38 +0200

    Added function to validate hmm folder

--- src/geomosaic/validator.py
+++ src/geomosaic/validator.py
@@ -4,9 +4,37 @@ import os
 
 def validate_working_dir(working_dir):
     if not os.path.exists(working_dir):
-        print(f"Inserted wdir path '{working_dir}' does not exists.")
+        print(f"GeoMosaic Error: Inserted wdir path '{working_dir}' does not exists.")
         exit(code=1)
 
     if not os.path.isdir(working_dir):
-        print(f"{working_dir} is not a directory")
+        print(f"GeoMosaic Error: {working_dir} is not a directory")
         exit(code=1)
+
+
+def validator_hmms_folder(hmms_folder):
+    if not os.path.exists(hmms_folder):
+        print(f"GeoMosaic Error: inserted folder does not exists.")
+        return False
+
+    if not os.path.isdir(hmms_folder):
+        print(f"GeoMosaic Error: '{hmms_folder}' is not a directory")
+        return False
+    
+    unique = []
+    for f in os.listdir(hmms_folder):
+        if not f.endswith(".hmm"):
+            print(f"GeoMosaic Error: The file:\n\n'{f}'\n\n inside the folder:\n\n'{hmms_folder}'\n\ndoesn't have the '.hmm' extension.")
+            return False
+        
+        if " " in f:
+            print(f"GeoMosaic Error: The file:\n\n'{f}'\n\n inside the folder:\n\n'{hmms_folder}'\n\nseems to have a space in its name.")
+            return False
+        
+        if f in unique:
+            print(f"GeoMosaic Error: The file:\n\n'{f}'\n\n inside the folder:\n\n'{hmms_folder}'\n\nseems to be a duplicate.")
+            return False
+
+        unique.append(f)
+
+    return True    
