f94409e7674eb62a384b24e1281c7567fd2738aa
davidecrs <davidecrs92@gmail.com>
2023-09-21 16:22:44 +0200

    Updated GeoMosaic labels

--- src/geomosaic/_utils.py
+++ src/geomosaic/_utils.py
@@ -1,7 +1,19 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-GEOMOSAIC_DESCRIPTION = "GeoMosaic: A flexible metagenomic pipeline combining read-based, assemblies and MAGs with downstream analysis"
+GEOMOSAIC_DESCRIPTION = f"{bcolors.BOLD}GeoMosaic: A flexible metagenomic pipeline combining read-based, assemblies and MAGs with downstream analysis{bcolors.ENDC}"
 
-GEOMOSAIC_ERROR = "GeoMosaic Error"
-GEOMOSAIC_WARNING = "GeoMosaic Warning"
+GEOMOSAIC_ERROR     = f"{bcolors.FAIL}GeoMosaic Error{bcolors.ENDC}"
+GEOMOSAIC_WARNING   = f"{bcolors.WARNING}GeoMosaic Warning{bcolors.ENDC}"
+GEOMOSAIC_NOTE      = f"{bcolors.OKBLUE}GeoMosaic Note{bcolors.ENDC}"
+GEOMOSAIC_PROCESS   = f"{bcolors.HEADER}GeoMosaic Process{bcolors.ENDC}"
 
-GEOMOSAIC_NOTE = "GeoMosaic Note"
+GEOMOSAIC_OK        = f"{bcolors.OKGREEN}OK{bcolors.ENDC}"
