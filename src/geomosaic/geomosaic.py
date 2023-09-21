96452fa060fd037af8d56ae48c539a99dc3890d0
davidecrs <davidecrs92@gmail.com>
2023-09-21 16:25:28 +0200

    Updated main commands of geomosaic

--- src/geomosaic/geomosaic.py
+++ src/geomosaic/geomosaic.py
@@ -4,6 +4,7 @@ from geomosaic.geo_infer import geo_infer
 from geomosaic.geo_setup import geo_setup
 from geomosaic.geo_workflow import geo_workflow
 from geomosaic.geo_unit import geo_unit
+import sys
 
 
 def main():
@@ -13,52 +14,75 @@ def main():
     subparsers = parser.add_subparsers(title="commands")
 
     # Creating subcommands for geomosaic
-    infer_parser = subparsers.add_parser("infer", help="this command it will help the user to create a \
-                                         sample table needed for geomosaic to prepare the working directory",
-                                         formatter_class=ArgumentDefaultsHelpFormatter)
+    # infer_parser = subparsers.add_parser("infer", help="this command it will help the user to create a \
+    #                                      sample table needed for geomosaic to prepare the working directory",
+    #                                      formatter_class=ArgumentDefaultsHelpFormatter)
     setup_parser = subparsers.add_parser("setup", help="this command creates the working directory for geomosaic, \
                                          and the relative sample folder based on the sample table provided by the user",
-                                         formatter_class=ArgumentDefaultsHelpFormatter)
+                                         formatter_class=ArgumentDefaultsHelpFormatter,
+                                         add_help=False)
     workflow_parser = subparsers.add_parser("workflow", help="this command allow the user to choose the desired \
                                             modules and the relative packages, then it will create a graph of the modules and start the execution",
-                                         formatter_class=ArgumentDefaultsHelpFormatter)
+                                         formatter_class=ArgumentDefaultsHelpFormatter,
+                                         add_help=False)
     unit_parser = subparsers.add_parser("unit", help="this command allow the user to choose and run just one module \
                                         in order execute an alternative package for that module",
-                                         formatter_class=ArgumentDefaultsHelpFormatter)
+                                         formatter_class=ArgumentDefaultsHelpFormatter,
+                                         add_help=False)
+    run_parser = subparsers.add_parser("run", help="Execute the create snakefile",
+                                         formatter_class=ArgumentDefaultsHelpFormatter,
+                                         add_help=False)
     
 
     # Adding parameters for each subcommamd
     ######################
     ## INFER Parameters ##
     ######################
-    infer_parser.add_argument("-d", "--directory", required=True, type=str, help="Path to the directory containing raw reads (fastq.gz files)")
-    infer_parser.add_argument("-o", "--output_file", required=False, type=str, default="geomosaic_sample_table.xlsx", help="Output filenameof the sample table")
-    infer_parser.add_argument("-s", "--split_token", required=False, type=str, default="_R1_,_R2_", 
-                        help="Part of the string filename that help to recognize paired end reads.\
-                        It need to be a string, comma separated with no space and two occurrences e.g _R1,_R2  or  R1_,R2_ .\
-                        It is also recommended to don't use 'R1' and 'R2' without any symbol, \
-                        cause 'R1' and 'R2' can occur also in the section of the filename that means the filename.")
-    ## INFER set defaut function
-    infer_parser.set_defaults(func=geo_infer)
+    # infer_parser.add_argument("-d", "--directory", required=True, type=str, help="Path to the directory containing raw reads (fastq.gz files)")
+    # infer_parser.add_argument("-o", "--output_file", required=False, type=str, default="geomosaic_sample_table.xlsx", help="Output filenameof the sample table")
+    # infer_parser.add_argument("-s", "--split_token", required=False, type=str, default="_R1_,_R2_", 
+    #                     help="Part of the string filename that help to recognize paired end reads.\
+    #                     It need to be a string, comma separated with no space and two occurrences e.g _R1,_R2  or  R1_,R2_ .\
+    #                     It is also recommended to don't use 'R1' and 'R2' without any symbol, \
+    #                     cause 'R1' and 'R2' can occur also in the section of the filename that means the filename.")
+    # ## INFER set defaut function
+    # infer_parser.set_defaults(func=geo_infer)
 
     ######################
-    ## SETUP Parameters ##
+    ## SETUP Parameters ## 
     ######################
-    setup_parser.add_argument("-d", "--directory", required=True, type=str, help="Path to the directory containing raw reads (fastq.gz files)")
-    setup_parser.add_argument("-s", "--sample_table", required=True, type=str, help="Path to the user sample table")
-    setup_parser.add_argument("-w", "--working_dir", required=False, default=".", type=str, help="Path where geomosaic can create its working directory")
-    setup_parser.add_argument("-c", "--config_file", required=False, default="geomosaic_setup_config.yaml", type=str, 
-                              help="Output name for the geomosaic config file (yaml extension). \
+    setup_required = setup_parser.add_argument_group("Required Arguments")
+    setup_required.add_argument("-d", "--directory", required=True, type=str, help="Path to the directory containing raw reads (fastq.gz files)")
+    setup_required.add_argument("-t", "--sample_table", required=True, type=str, help="Path to the user sample table")
+
+    setup_optional = setup_parser.add_argument_group("Optional Arguments")
+    setup_optional.add_argument("-s", "--setup_file", required=False, default="gmsetup.yaml", type=str, 
+                              help="Output name for the geomosaic setup file (yaml extension). \
                                 This file is necessary for the < geomosaic workflow > command.")
+    setup_optional.add_argument("-w", "--working_dir", required=False, default="geomosaic", type=str, help="Path where geomosaic can create its working directory")
+    setup_optional.add_argument("-n", "--project_name", required=False, default="GeoMosaic_Workflow", type=str, help="Name of the project (no-space)")
+    setup_optional.add_argument('--skip_wdir_checks', action='store_true', required=False,  help="")
+    setup_optional.add_argument('--nocopy', action='store_true', required=False,  help="Suggested flag if the provided raw reads directory is an already backup of the original files. \
+                              In this case, geomosaic will create only symbolic link of raw reads to its working directory. Note: This flag cannot be used if \
+                              there are multiple files for each R1 and R2 sample reads, as geomosaic will 'cat' them to a single file.")
+    
+    setup_help = setup_parser.add_argument_group("Help Arguments")
+    setup_help.add_argument("-h", "--help", action="help", help="show this help message and exit")
     ## SETUP set default function
     setup_parser.set_defaults(func=geo_setup)
 
     #########################
     ## WORKFLOW Parameters ##
     #########################
-    workflow_parser.add_argument("-c", "--config_file", required=True, default="geomosaic_setup_config.yaml", type=str, 
-                              help="Path and output filename for the geomosaic config file (yaml extension).")
-    workflow_parser.add_argument('-p' ,'--pipeline', action='store_true', help="Execute the default pipeline of geomosaic.")
+    workflow_required = workflow_parser.add_argument_group("Required Arguments")
+    workflow_required.add_argument("-s", "--setup_file", required=True, type=str, 
+                              help="Geomosaic setup file created from the 'geomosaic setup ...' command.")
+    
+    workflow_optional = workflow_parser.add_argument_group("Optional Arguments")
+    workflow_optional.add_argument('-p' ,'--pipeline', action='store_true', help="Execute the default pipeline of geomosaic.")
+    
+    workflow_help = workflow_parser.add_argument_group("Help Arguments")
+    workflow_help.add_argument("-h", "--help", action="help", help="show this help message and exit")
     ## WORKFLOW set default function
     workflow_parser.set_defaults(func=geo_workflow)
 
@@ -68,9 +92,22 @@ def main():
     unit_parser.add_argument("-c", "--config_file", required=True, default="geomosaic_setup_config.yaml", type=str, 
                               help="Path and output filename for the geomosaic config file (yaml extension).")
     unit_parser.add_argument("-m", "--module", required=True, default=None, type=str, 
-                              help="Path and output filename for the geomosaic config file (yaml extension).")
-    ## WORKFLOW set default function
+                              help="Modules to execute")
+    ## UNIT set default function
     unit_parser.set_defaults(func=geo_unit)
+
+    ####################
+    ## RUN Parameters ##
+    ####################
+    run_parser.add_argument("-c", "--config_file", required=True, default="geomosaic_setup_config.yaml", type=str, 
+                              help="Path and output filename for the geomosaic config file (yaml extension).")
+    run_parser.add_argument("-t", "--threads", required=True, default=8, help="Number of threads to use to execute the workflow for all the samples")
+    run_parser.add_argument("-s", "--threads_per_sample", required=True, default=4, help="Threads to use per each sample. It must be lower than the 'threads' \
+                            parameter")
+
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
     args = parser.parse_args()
     args.func(args)
