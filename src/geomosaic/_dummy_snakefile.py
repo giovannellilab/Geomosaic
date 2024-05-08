77988a9b06325d72cf6464b6f546fccb42c5763c
davidecrs <davidecrs92@gmail.com>
2024-05-08 20:10:47 +0200

    Updated dummy snakefile

--- src/geomosaic/_dummy_snakefile.py
+++ src/geomosaic/_dummy_snakefile.py
@@ -1,28 +1,50 @@
 import yaml
 
-def dummy_rule(package, condaenv):
+
+def dummy_rule(geomosaic_wdir, package, condaenv):
+    outfilename = "{wdir}/geomosaic_temp_{package}.txt".format(wdir=geomosaic_wdir, package=package)
+
     dummy_rule = """
 
 rule dummy_{package}:
     output:
-        file=temp("test_{package}.txt")
+        file=temp("{outfilename}")
     conda:
         {condaenv}
     shell:
         """
-    s = dummy_rule.format(package=package, condaenv=condaenv) +'"touch {output.file}"' + "\n\n"
+    s = dummy_rule.format(outfilename=outfilename, condaenv=condaenv) +'"touch {output.file}"' + "\n\n"
+    return s, outfilename
+
+
+def make_inputs_allrule(list_outfilenames):
+    s = ""
+    for f in list_outfilenames:
+        if f == list_outfilenames[-1]:
+            s += f"\t\t{f}\n"
+        else:
+            s += f"\t\t{f},\n\n\n"
+    
     return s
 
-def dummy_snakefile(config_env, configpath):
-    dummy_snakefile = """
-configfile: '{configpath}'
 
-""".format(configpath=configpath)
+def dummy_snakefile(config_env, configpath):
+    all_rules = ""
+    all_outfilenames = []
 
     for pckg, _ in config_env.items():
         prefix = f'config["ENVS"]["{pckg}"]'
-        dummy_snakefile += dummy_rule(pckg, prefix)
+        dr, of = dummy_rule(pckg, prefix)
+        all_rules += dr
+        all_outfilenames.append(of)
 
+    dummy_snakefile = """
+configfile: '{configpath}'
+
+rule all:
+    input:
+""".format(configpath=configpath) + make_inputs_allrule(all_outfilenames) + all_rules
+    
     return dummy_snakefile
 
 
