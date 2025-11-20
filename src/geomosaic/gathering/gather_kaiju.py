509f4080a25703c2c43c76b9e0f3121267c40e51
EdoT99 <edoardotaccaliti@gmail.com>
2025-11-20 11:42:48 +0100

    minor refactor & get samples changed

--- src/geomosaic/gathering/gather_kaiju.py
+++ src/geomosaic/gathering/gather_kaiju.py
@@ -7,48 +7,61 @@ import yaml
 from geomosaic.gathering.utils import get_sample_with_results
 
 
-def gather_kaiju(config_file, geomosaic_wdir, output_base_folder, additional_info):
+def gather_kaiju(all_samples, geomosaic_wdir, output_base_folder, additional_info):
     pckg = "kaiju"
-
-    with open(config_file) as file:
-        config = yaml.load(file, Loader=yaml.FullLoader)
-    
-    samples = get_sample_with_results(pckg, geomosaic_wdir, config["SAMPLES"])
+ 
+    samples = get_sample_with_results(pckg, geomosaic_wdir, all_samples)
 
     output_folder = os.path.join(output_base_folder, pckg)
 
     check_call(f"mkdir -p {output_folder}", shell=True)
-    compose_matrix_kaiju(geomosaic_wdir, output_folder, samples)
+    compose_matrix_kaiju(geomosaic_wdir, output_folder, samples, pckg)
 
 
-def compose_matrix_kaiju(folder, output_folder, samples):
+def compose_matrix_kaiju(folder, output_folder, samples, pckg):
     for t in ["phylum", "class", "order", "family", "genus", "species"]:
         pivot = t
         unique_list = set()
-        list_dfs = []
+        list_dfs_percent = []
+        list_dfs_reads = []
 
         for s in samples:
-            folder_data = f"{folder}/{s}/kaiju"
+            folder_data = os.path.join(folder,s,pckg)
             
             flag = True
             if f"{t}.tsv" not in listdir(folder_data):
                 flag = False
                 break
 
-            rawdf = pd.read_csv(f"{folder_data}/{t}.tsv", sep="\t")
-            df = rawdf.loc[:, ["taxon_name", "percent"]]
-            df.rename(columns={"taxon_name": pivot, "percent": s}, inplace=True)
+            rawdf = pd.read_csv(os.path.join(folder_data,f"{t}.tsv"), sep="\t")
             
-            unique_list.update(list(df[pivot].unique()))
-            list_dfs.append(df)
+            df_percent = rawdf.loc[:, ["taxon_name", "percent"]]
+            df.rename(columns={"taxon_name": pivot, "percent": s}, inplace=True)
+
+            df_reads = rawdf.loc[:, ["taxon_name", "reads"]]
+            df_reads.rename(columns={"taxon_name": pivot, "reads": s}, inplace=True)
+            
+            unique_list.update(list(df_percent[pivot].unique()))
+            
+            list_dfs_percent.append(df_percent)
+            list_dfs_reads(df_reads)
 
         if not flag:
             continue
 
-        m = pd.DataFrame(sorted(unique_list), columns=[pivot])
-        for x in list_dfs:
-            temp = pd.merge(m, x, how="left", on=pivot)
-            m = temp.copy()
-        
-        finalm = m.replace(np.nan, 0, regex=True)
-        finalm.to_csv(f"{output_folder}/{t}.tsv", sep="\t", index=False, header=True)
+        m_percent = pd.DataFrame(sorted(unique_list), columns=[pivot])
+        m_reads = pd.DataFrame(sorted(unique_list), columns=[pivot])
+
+        for x in list_dfs_percent:
+            temp_1 = pd.merge(m_, x, how="left", on=pivot)
+            m = temp_1.copy()
+        for x in list_dfs_reads:
+            temp_2 = pd.merge(m_reads, x, how="left", on=pivot)
+            m_reads = temp_2.copy()
+
+        finalm_ = m.replace(np.nan, 0, regex=True)
+        finalm.to_csv(os.path.join(output_folder,f"{t}.tsv"), sep="\t", index=False, header=True)
+
+        finalm_reads = m_reads.replace(np.nan, 0, regex=True)
+        finalm_reads.to_csv(os.path.join(output_folder,f"{t}_reads.tsv"), sep="\t", index=False, header=True)
+
